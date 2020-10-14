a075250f066e862583cc5453b45c4708ab7c2b0d
Todd Greenwood <pub+github@zwrob.com>
2020-10-14 05:48:56 -0700

    uno - add serial logging [ufmt]

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}-bin/src/main.rs
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}-bin/src/main.rs
@@ -4,11 +4,25 @@
 // Pull in the panic handler from panic-halt
 extern crate panic_halt;
 
-//use arduino_uno::prelude::*;
+use arduino_uno::prelude::*;
 
 #[arduino_uno::entry]
 fn main() -> ! {
-    let _dp = arduino_uno::Peripherals::take().unwrap();
+    let dp = arduino_uno::Peripherals::take().unwrap();
 
-    unimplemented!()
+    let mut pins = arduino_uno::Pins::new(dp.PORTB, dp.PORTC, dp.PORTD);
+
+    let mut serial = arduino_uno::Serial::new(
+        dp.USART0,
+        pins.d0,
+        pins.d1.into_output(&mut pins.ddr),
+        9600,
+    );
+
+    ufmt::uwriteln!(&mut serial, "Starting main loop...\r").void_unwrap();
+    loop {
+        ufmt::uwriteln!(&mut serial, "main loop...\r").void_unwrap();
+        arduino_uno::delay_ms(1000);
+    }
 }
+
